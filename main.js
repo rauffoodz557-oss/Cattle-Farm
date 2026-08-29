068c51abc0d2159d95db90301c138b7bcad12c16
rauffoodz557-oss <rauffoodz557@gmail.com>
2026-08-29 10:57:14 +0500

    Add IPC print handling to main process

--- main.js
+++ main.js
@@ -1,12 +1,19 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, ipcMain } = require('electron');
+const path = require('path');
+
+let win;
 
 function createWindow () {
-  const win = new BrowserWindow({
+  win = new BrowserWindow({
     width: 800,
     height: 600,
-    autoHideMenuBar: true
+    autoHideMenuBar: true,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+      contextIsolation: true,
+      nodeIntegration: false
+    }
   });
-
   win.loadFile('index.html');
 }
 
@@ -14,4 +21,22 @@ app.whenReady().then(createWindow);
 
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') app.quit();
+});
+
+// Renderer (app.js via preload) asks main process to print.
+// silent:false forces the native OS print dialog to appear,
+// where the user can pick printer, paper size (A4/A6) and scale.
+ipcMain.handle('do-print', () => {
+  return new Promise((resolve) => {
+    if (!win) return resolve(false);
+    win.webContents.print(
+      {
+        silent: false,
+        printBackground: true
+      },
+      (success, errorType) => {
+        resolve({ success, errorType });
+      }
+    );
+  });
 });
